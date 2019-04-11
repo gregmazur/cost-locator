0470bfd47e45d2594b2e66fdc238cca7f6b02f03
gregmazur <grygorii.mazur@hys-enterprise.com>
2019-04-11 16:16:08 +0300

    extracted launch of street reader from scheduler

--- src/main/java/org/open/budget/costlocator/Scheduler.java
+++ src/main/java/org/open/budget/costlocator/Scheduler.java
@@ -1,14 +1,11 @@
 package org.open.budget.costlocator;
 
-import org.open.budget.costlocator.service.StreetService;
 import org.open.budget.costlocator.service.TenderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 
 @Component
@@ -16,21 +13,11 @@ public class Scheduler {
     private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
 
     @Autowired
-    private StreetService streetService;
-    @Value( "${need.to.read.csv:true}" )
-    private boolean needToReadStreetsFile;
-
-    @Autowired
     private TenderService tenderService;
 
-    @Scheduled(cron = "0 0 23 * * *")
+    @Scheduled(cron = "0 0 23 ? * FRI")
     public void loadTenders() {
         log.warn("ATTENTION---STARTED TASK----------ATTENTION---------------ATTENTION---------------------");
-        if (needToReadStreetsFile) {
-            StreetReaderCsv readerServiceCsv = new StreetReaderCsv(streetService);
-            log.warn("ATTENTION---STARTED READ FROM HOUSES----------ATTENTION---------------ATTENTION---------------------");
-            readerServiceCsv.start();
-        }
         Extractor extractor = new Extractor(tenderService);
         extractor.extract();
     }
